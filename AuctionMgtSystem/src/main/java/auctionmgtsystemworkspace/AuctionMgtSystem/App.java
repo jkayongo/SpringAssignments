a05ae1e245baee04a03029930961aff9a1c0d2fa
jkayongo <jkayongo.kj@gmail.com>
2025-01-24 20:46:38 +0300

    initial commit of the auction management system.

---
+++ AuctionMgtSystem/src/main/java/auctionmgtsystemworkspace/AuctionMgtSystem/App.java
@@ -0,0 +1,13 @@
+package auctionmgtsystemworkspace.AuctionMgtSystem;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
